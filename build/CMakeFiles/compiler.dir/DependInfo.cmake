7bbaf481d46dfb5054207b85677db0401d317bb8
某科学的编译系统 <1007641546@qq.com>
2022-09-19 20:11:27 +0800

    Modify the architecture

---
+++ build/CMakeFiles/compiler.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fzcsyglq/桌面/only-my-compiler/src/frontend/SysYBaseListener.cpp" "/home/fzcsyglq/桌面/only-my-compiler/build/CMakeFiles/compiler.dir/src/frontend/SysYBaseListener.cpp.o"
+  "/home/fzcsyglq/桌面/only-my-compiler/src/frontend/SysYBaseVisitor.cpp" "/home/fzcsyglq/桌面/only-my-compiler/build/CMakeFiles/compiler.dir/src/frontend/SysYBaseVisitor.cpp.o"
+  "/home/fzcsyglq/桌面/only-my-compiler/src/frontend/SysYLexer.cpp" "/home/fzcsyglq/桌面/only-my-compiler/build/CMakeFiles/compiler.dir/src/frontend/SysYLexer.cpp.o"
+  "/home/fzcsyglq/桌面/only-my-compiler/src/frontend/SysYListener.cpp" "/home/fzcsyglq/桌面/only-my-compiler/build/CMakeFiles/compiler.dir/src/frontend/SysYListener.cpp.o"
+  "/home/fzcsyglq/桌面/only-my-compiler/src/frontend/SysYParser.cpp" "/home/fzcsyglq/桌面/only-my-compiler/build/CMakeFiles/compiler.dir/src/frontend/SysYParser.cpp.o"
+  "/home/fzcsyglq/桌面/only-my-compiler/src/frontend/SysYVisitor.cpp" "/home/fzcsyglq/桌面/only-my-compiler/build/CMakeFiles/compiler.dir/src/frontend/SysYVisitor.cpp.o"
+  "/home/fzcsyglq/桌面/only-my-compiler/src/frontend/main.cpp" "/home/fzcsyglq/桌面/only-my-compiler/build/CMakeFiles/compiler.dir/src/frontend/main.cpp.o"
+  "/home/fzcsyglq/桌面/only-my-compiler/src/frontend/visitor.cpp" "/home/fzcsyglq/桌面/only-my-compiler/build/CMakeFiles/compiler.dir/src/frontend/visitor.cpp.o"
+  "/home/fzcsyglq/桌面/only-my-compiler/src/ir/ir.cpp" "/home/fzcsyglq/桌面/only-my-compiler/build/CMakeFiles/compiler.dir/src/ir/ir.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/compiler.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler.dir/src/frontend/SysYBaseListener.cpp.o"
+  "CMakeFiles/compiler.dir/src/frontend/SysYBaseVisitor.cpp.o"
+  "CMakeFiles/compiler.dir/src/frontend/SysYLexer.cpp.o"
+  "CMakeFiles/compiler.dir/src/frontend/SysYListener.cpp.o"
+  "CMakeFiles/compiler.dir/src/frontend/SysYParser.cpp.o"
+  "CMakeFiles/compiler.dir/src/frontend/SysYVisitor.cpp.o"
+  "CMakeFiles/compiler.dir/src/frontend/main.cpp.o"
+  "CMakeFiles/compiler.dir/src/frontend/visitor.cpp.o"
+  "CMakeFiles/compiler.dir/src/ir/ir.cpp.o"
+  "compiler"
+  "compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
